649296e6e329a193ca6d19952782e4887352fe68
Ness-uno <nessuno@caianiello.it>
2016-12-01 11:31:35 +0000

    Add doc about Sidekiq

--- app/workers/like_hate_email_worker.rb
+++ app/workers/like_hate_email_worker.rb
@@ -2,6 +2,10 @@ class LikeHateEmailWorker
   include Sidekiq::Worker
 
   def perform(json_params)
+    # The Sidekiq client API uses JSON.dump to send the data to Redis.
+    # The Sidekiq server pulls that JSON data from Redis and uses JSON.load
+    # to convert the data back into Ruby types to pass to your perform method.
+
     params = LikeHateEmailParams.from_json(json_params)
     UserMailer.like_hate_email(params).deliver
   end
