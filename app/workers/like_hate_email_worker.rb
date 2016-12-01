041db0726986cac85dede1c34456bb4f02a1d9b3
Ness-uno <nessuno@caianiello.it>
2016-12-01 10:50:32 +0000

    Update LikeHateEmailWorker to deserialise the job payload

--- app/workers/like_hate_email_worker.rb
+++ app/workers/like_hate_email_worker.rb
@@ -1,8 +1,9 @@
 class LikeHateEmailWorker
   include Sidekiq::Worker
 
-  def perform(email_params)
-    UserMailer.like_hate_email(email_params).deliver
+  def perform(json_params)
+    params = LikeHateEmailParams.from_json(json_params)
+    UserMailer.like_hate_email(params).deliver
   end
 
 end
--- spec/workers/like_hate_email_worker_spec.rb
+++ spec/workers/like_hate_email_worker_spec.rb
@@ -2,14 +2,18 @@ require 'rails_helper'
 
 RSpec.describe LikeHateEmailWorker do
   describe '#perform' do
+    let(:json_params) { "{}" }
     let(:email_params) { double('EmailParams') }
     let(:mailer) { double('Mailer', deliver: 'OK') }
 
-    before { allow(UserMailer).to receive(:like_hate_email).with(email_params).and_return(mailer) }
+    before do
+      allow(LikeHateEmailParams).to receive(:from_json).with(json_params).and_return(email_params)
+      allow(UserMailer).to receive(:like_hate_email).with(email_params).and_return(mailer)
+    end
 
     it 'delivers a like_email' do
       expect(mailer).to receive(:deliver).and_return('OK')
-      subject.perform(email_params)
+      subject.perform(json_params)
     end
   end
 end
